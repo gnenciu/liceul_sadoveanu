c4847f8928b4b70bfa0cbff6b03aea1f69119088
gnenciu <74288747+gnenciu@users.noreply.github.com>
2021-09-24 07:19:38 +0300

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Liceul Teoretic Mihail Sadoveanu</title>
+	<?php include("html/head.html");?>
+</head>
+
+<body>
+	<div class="container">
+	<?php $page="contact";?>
+	<?php include("html/header.php");?>
+	<?php include("html/contact.html");?>
+	<?php include("html/footer.html");?>
+	</div>
+</body>
+</html>
